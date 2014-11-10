7a4e434cb87f7d4a8899c79566519fb499c6c7f9
rachot <chot.embedded@gmail.com>
2014-11-10 18:09:28 +0700

    Rev.01

--- pHController.ino
+++ pHController.ino
@@ -13,24 +13,13 @@ float sensorValueOld;
 float voltage;
 float pH;
 float senSorValueSum;
+const float pump_feed = 4.347826; // ml/sec
 
 int pHcount=0;
 
-void setup() {
-  Serial.begin(9600); 
-   //  int tickEvent = t.every(10800000, doSomething); // every 3 hours
-   int tickEvent = t.every(3600000, doSomething);   // every 1 hour
-   pinMode(7, OUTPUT);  
-   digitalWrite(7, HIGH);
-
-  lcd.begin(20, 4);
-  lcd.print("  pH Controller  ");
-}
-
-void loop() {
-  t.update();
-  
-  senSorValueSum = 0;
+void sensor_Read()
+{
+    senSorValueSum = 0;
   
   for(int l=1;l<=100;l++)
   {
@@ -40,7 +29,27 @@ void loop() {
   }
 
 sensorValue =senSorValueSum/100;  
-pH = 7.3 - ((0.026315789473684210526315789473684*sensorValue)-12.421052631578947368421052631579);
+pH = 7.8 - ((0.026315789473684210526315789473684*sensorValue)-12.421052631578947368421052631579);
+
+}
+
+void setup() {
+  Serial.begin(9600); 
+     int tickEvent = t.every(10800000, doSomething); // every 3 hours
+   //int tickEvent = t.every(3600000, doSomething);   // every 1 hour
+   //int tickEvent = t.every(1000, doSomething);   // every 10 seconds
+   pinMode(7, OUTPUT);  
+   digitalWrite(7, HIGH);
+
+  lcd.begin(20, 4);
+  lcd.print("  pH Controller  ");
+  
+}
+
+void loop() {
+  t.update();
+  
+sensor_Read();
 
   Serial.print("pH = " );                       
   Serial.print(pH,1);      
@@ -48,30 +57,64 @@ pH = 7.3 - ((0.026315789473684210526315789473684*sensorValue)-12.421052631578947
   Serial.println(sensorValue);
   delay(50);             
 
-  lcd.setCursor(0, 1);
+    lcd.setCursor(0, 1);
+      lcd.print("Monitoring Mode");
+  lcd.setCursor(0, 2);
   lcd.print("pH = ");
   lcd.print(pH,1);
-  delay(1000);
+  lcd.print("  EC = --");
+  lcd.setCursor(0, 3);
+  lcd.print("Temp = --");
+  //lcd.setCursor(0, 2);
+  //lcd.print("ADC = ");
+  //lcd.print(sensorValue);
+
+  
+  delay(3000);
      
 }
 
-void doSomething()
+void doSomething() // do after tick 
 {
-  Serial.print("pH = " );                       
-  Serial.print(pH,1);      
-  Serial.print("   ADC = " );                       
-  Serial.println(sensorValue);
-
-  float ml_feed=0;
-  ml_feed = (pH - 6.0)/0.033;
-  
-
-  if(sensorValue<=441)
+  sensor_Read();
+  //while(sensorValue<=515)  // pH = 6.2
+  while(pH>6.2)  // pH = 6.2
   {
-    Serial.println("pH = 6.5 Feed water 50 ml.");  
-    digitalWrite(7, LOW);
-    //delay(12000); 
-    digitalWrite(7, HIGH);
+    
+    float ml_feed=0;
+    float pump_time=0;
+    ml_feed = (pH - 6.0)/0.009;
+    pump_time = ml_feed/pump_feed;
+    
+      Serial.print("pH = " );                       
+      Serial.print(pH,1);      
+      Serial.print("   ADC = " );                       
+      Serial.println(sensorValue);
+      Serial.print("Feed water = ");
+      Serial.print(ml_feed);
+      Serial.print("\r\n");
+      Serial.print("Pump time = ");
+      Serial.print((int)pump_time);
+      Serial.print("\r\n");
+      
+      digitalWrite(7, LOW);
+      delay((int)pump_time*1000); 
+      digitalWrite(7, HIGH);
+      
+      // wait for mixing time
+      Serial.print("Mixing....");
+      Serial.print("\r\n");
+ 
+      lcd.setCursor(0, 3);
+      lcd.print("Mixing...");
+  
+      for(int co=0;co<900;co++)
+        delay(1000);
+        
+     Serial.print("Finished Mixing");
+        
+         sensor_Read();
   }
+  Serial.print("pH Ready...");
 }
 
